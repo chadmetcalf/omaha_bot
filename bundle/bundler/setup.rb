6cb1440a75b8e00d5c74b1200a8c25a2faf1c38f
Chad Metcalf <cm0591@gmail.com>
2014-07-12 01:14:48 -0500

    Bundler Package config

--- bundle/bundler/setup.rb
+++ bundle/bundler/setup.rb
@@ -1,7 +1,7 @@
-require 'rbconfig'
+# require 'rbconfig'
 # ruby 1.8.7 doesn't define RUBY_ENGINE
-ruby_engine = defined?(RUBY_ENGINE) ? RUBY_ENGINE : 'ruby'
-ruby_version = RbConfig::CONFIG["ruby_version"]
+ruby_engine = 'ruby'
+ruby_version = '2.1.0'
 path = File.expand_path('..', __FILE__)
 $:.unshift File.expand_path("#{path}/../#{ruby_engine}/#{ruby_version}/gems/darwinning-0.0.2/lib")
 $:.unshift File.expand_path("#{path}/../#{ruby_engine}/#{ruby_version}/gems/dotenv-deployment-0.0.2/lib")
